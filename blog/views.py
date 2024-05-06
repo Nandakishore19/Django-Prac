0df40a6edd8d7b4880267f950d048a4866ce45c6
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-06 10:52:26 +0530

    Ordering of posts

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     paginate_by = 5
+    ordering = ['-date_posted']
     #<app>/<mode>_<viewtype>.html
 
 class UserPostListView(ListView):
