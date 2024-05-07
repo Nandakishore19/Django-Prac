c2309b3d2ca668aebd88bfaac0c770d13910186b
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-07 17:34:14 +0530

    App:Blog

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)  #auto_now = True would update the post everytime the poast is updated or auto_add_now will set time to the time when post was created
     author = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
