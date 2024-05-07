6dfa6dbab900ef73cf12172cd9825a0279dd8cc0
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-07 10:54:21 +0530

    Added Email Regex

--- users/models.py
+++ users/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.core.validators import RegexValidator
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    email = models.EmailField(validators=[RegexValidator(
+        regex=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+        message='Please enter a valid email address',
+        code='invalid_email'
+    )],
+    default = ""
+    )
 
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile,self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
