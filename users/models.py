24f64cae92af8c34d5f633e18a8994dcdcab7755
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-07 17:35:28 +0530

    App:Users

--- users/models.py
+++ users/models.py
@@ -2,26 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from django.core.validators import RegexValidator
+
 # Create your models here.
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
-    email = models.EmailField(validators=[RegexValidator(
-        regex=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-        message='Please enter a valid email address',
-        code='invalid_email'
-    )],
-    default = ""
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    email = models.EmailField(
+        validators=[
+            RegexValidator(
+                regex=r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+                message="Please enter a valid email address",
+                code="invalid_email",
+            )
+        ],
+        default="",
     )
 
     def __str__(self) -> str:
-        return f'{self.user.username} Profile'
+        return f"{self.user.username} Profile"
+
     def save(self, *args, **kwargs):
-        super(Profile,self).save(*args, **kwargs)
+        super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
-            output_size = (300,300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
